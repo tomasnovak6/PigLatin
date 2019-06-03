e5d9c0d14dc085d0edbeef6e40c2bbd604370c03
Tomas Novak <ext.Tomas.Novak5@skoda-auto.cz>
2019-06-03 21:00:41 +0200

    Init commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import * as React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+
+}
+interface AppState {
+
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  render() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <section>
+
+            </section>
+            <footer>
+              Autor: Tomáš Novák, 2019
+            </footer>
+        </div>
+    );
+  }
+
 }
 
 export default App;
