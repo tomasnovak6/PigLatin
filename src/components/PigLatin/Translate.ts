d6cc4defb45588c13a6cc75218103f69b17e2f37
Tomas Novak <ext.Tomas.Novak5@skoda-auto.cz>
2019-06-07 19:35:17 +0200

    Hyphens implemented.

--- src/components/PigLatin/Translate.ts
+++ src/components/PigLatin/Translate.ts
@@ -1,7 +1,9 @@
 class Translate {
 
     private inputValue: string = '';
+    private inputValueArr: string[] = [];
     private outputValue: string = '';
+    private outputValueArr: string[] = [];
 
     private consonants: string = 'bcdfghjklmnpqrstvwxyz';
     private vowels: string = 'aeiou';
@@ -14,12 +16,34 @@ class Translate {
         return this.inputValue;
     }
 
-    public getOutputValue(): string {
+    private getInputValueArr(): string[] {
+        return this.inputValueArr;
+    }
+
+    private getInputValueItem(i: number): string {
+        return this.inputValueArr[i];
+    }
+
+    private getOutputValue(): string {
         return this.outputValue;
     }
 
-    private setOutputValue(inputValue: string): void {
-        this.outputValue = inputValue;
+    private getOutputValueArr(): string[] {
+        return this.outputValueArr;
+    }
+
+    private setOutputValue() {
+        if (this.getOutputValueArr()) {
+            this.outputValue = this.getOutputValueArr().join('-');
+        }
+    }
+
+    private setInputValueArr(inputValueArr: string[]) {
+        this.inputValueArr = inputValueArr;
+    }
+
+    private setOutputValueArr(inputValue: string, i: number): void {
+        this.outputValueArr[i] = inputValue;
     }
 
     private getConsonants(): string {
@@ -40,71 +64,81 @@ class Translate {
         return result;
     }
 
-    private translateForConsonant(): void {
+    private translateForConsonant(i: number): void {
         let result: string = '';
-        result = this.getInputValue().substr(1, this.getInputValue().length) + this.getInputValue().substr(0, 1) + 'ay';
+        result = this.getInputValueItem(i).substr(1, this.getInputValueItem(i).length) + this.getInputValueItem(i).substr(0, 1) + 'ay';
         result = result.charAt(0).toUpperCase() + result.slice(1);
-        this.setOutputValue(result);
+        this.setOutputValueArr(result, i);
     }
 
-    private traslateForVowel(): void {
+    private traslateForVowel(i: number): void {
         let result: string = '';
-        result = this.getInputValue() + 'way';
-        this.setOutputValue(result);
+        result = this.getInputValueItem(i) + 'way';
+        this.setOutputValueArr(result, i);
     }
 
-    private translateSameResult(): void {
-        this.setOutputValue(this.getInputValue());
+    private translateSameResult(i: number): void {
+        this.setOutputValueArr(this.getInputValueItem(i), i);
     }
 
-    private translatePunctuation(): void {
+    private translatePunctuation(i: number): void {
         let result: string = '';
-        let punctuationPosition: number = this.getInputValue().indexOf('\'');
+        let punctuationPosition: number = this.getInputValueItem(i).indexOf('\'');
     }
-
-    private translateHyphens(): void {
-        let resultArr: string[] = [];
-
-        if (this.getInputValue().indexOf('-') > -1) {
-            resultArr = this.getInputValue().split('-');
-
-            if (resultArr.length > 0) {
-                for (let i in resultArr) {
-
-                }
-            }
-        }
-
-        console.log('resultArr', resultArr);
-    }
-
-    private translateCapitalization(): void {
+    
+    private translateCapitalization(i: number): void {
 
     }
 
     public runTranslating(): string {
-        let inputValue: string = this.getInputValue().toLowerCase();
-        let firstChar: string = inputValue.substr(0, 1);
-        let result: string = '';
+        let inputValueArr: string[] = [];
 
+        // pokud je to spojene z vice slov
+        if (this.getInputValue().indexOf('-') > -1) {
+            // pocatecni inicializace vstupnich promennych
+            inputValueArr = this.getInputValue().split('-');
+            // s timto se pracuje dale v prekladovych metodach
+            this.setInputValueArr(inputValueArr);
 
-        if (this.shouldBeChanged()) {
-            if (this.getConsonants().indexOf(firstChar) > -1) {
-                this.translateForConsonant();
-            } else if (this.getVowels().indexOf(firstChar) > -1) {
-                this.traslateForVowel();
+            if (inputValueArr.length > 0) {
+                for (let i = 0; i < inputValueArr.length; i++) {
+                    let inputValue: string = this.getInputValueItem(i).toLowerCase();
+                    let firstChar: string = this.getInputValueItem(i).substr(0, 1);
+
+                    // tady zacinam predkladat
+                    this.translateParticularWords(firstChar, i);
+                }
             }
-            // zkontroluje interpunkci
-            this.translatePunctuation();
 
-            // zkontroluje, ze tam neni vice slov oddelenych pomlckou
-            this.translateHyphens();
-
+        // pokud prekladam jen 1 slovo
         } else {
-            this.translateSameResult();
+            inputValueArr[0] = this.getInputValue();
+            this.setInputValueArr(inputValueArr);
+
+            let firstChar: string = this.getInputValueItem(0).substr(0, 1);
+
+            this.translateParticularWords(firstChar, 0);
         }
 
+        this.setOutputValue();
+
         return this.getOutputValue();
+    }
+
+    private translateParticularWords(firstChar: string, i: number): void {
+        if (this.shouldBeChanged()) {
+            if (this.getConsonants().indexOf(firstChar) > -1) {
+                this.translateForConsonant(i);
+            } else if (this.getVowels().indexOf(firstChar) > -1) {
+                this.traslateForVowel(i);
+            }
+            // zkontroluje interpunkci
+            this.translatePunctuation(i);
+            // zkontroluje velka a mala pismena
+            this.translateCapitalization(i);
+        } else {
+            this.translateSameResult(i);
+        }
     }
 
 }
